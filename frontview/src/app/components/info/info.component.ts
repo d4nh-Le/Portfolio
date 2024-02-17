6a0ae768773ce92b9680f16c7800061144286a3f
Danh Le <danhle002@gmail.com>
2024-02-16 22:33:59 -0700

    Frontview - connection components is adjusted and improved - open links in new tabs instead of going with current tab

--- frontview/src/app/components/info/info.component.ts
+++ frontview/src/app/components/info/info.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./info.component.css']
 })
 export class InfoComponent {
-
+  name: string = "Danh Le";
+  location: string = "Calgary, AB"
 }
