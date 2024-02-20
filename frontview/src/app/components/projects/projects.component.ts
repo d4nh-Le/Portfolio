131e2aeb6e3edce0d60bdb7f8898a7955b62a73c
Danh Le <danhle002@gmail.com>
2024-02-20 02:03:39 -0700

    frontview - added functional Project component

--- frontview/src/app/components/projects/projects.component.ts
+++ frontview/src/app/components/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Project } from './projects.projectsModel.component';
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  results: Project[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this.http.get<any>('http://localhost:3000/projects')
+      .subscribe(
+        (data: Project[]) => {
+          this.results = data.map((project: any) => ({
+            title: project.projectTitle,
+            author: project.projectAuthors,
+            technologies: project.technologies,
+            description: project.projectDescription,
+            date: project.projectDate,
+            images: project.projectImages
+          }));
+        },
+        error => {
+          console.error('Error:', error);
+        }
+      );
+  }
+
+  imageExists(imageUrl: string): boolean {
+    const http = new XMLHttpRequest();
+    http.open('HEAD', imageUrl, false);
+    http.send();
+    return http.status !== 404;
+  }
 }
