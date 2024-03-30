0c37f154f554c727f4917705595aca69c0d0d3a7
Danh Le <danhle002@gmail.com>
2024-03-29 23:47:09 -0600

    frontend - UI design v1.2 - Home Container page designs are updated

--- frontend/src/app/components/general-components/containers/home-container/home-container.component.ts
+++ frontend/src/app/components/general-components/containers/home-container/home-container.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MenuBarComponent } from '../../menu-bar/menu-bar.component';
 import { InfoComponent } from '../../../info/info.component';   
 import { SkillsComponent } from '../../../skills/skills.component';
+import { FooterBarComponent } from '../../footer-bar/footer-bar.component';
 
 @Component({
   selector: 'app-home-container',
   standalone: true,
-  imports: [MenuBarComponent, InfoComponent, SkillsComponent],
+  imports: [MenuBarComponent, InfoComponent, SkillsComponent, FooterBarComponent],
   templateUrl: './home-container.component.html',
   styleUrl: './home-container.component.css'
 })
