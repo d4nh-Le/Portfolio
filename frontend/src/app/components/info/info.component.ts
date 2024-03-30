c816d5f99b5ca048ccf8fc36f37b9e18fe6c9977
Danh Le <danhle002@gmail.com>
2024-03-29 19:03:23 -0600

    frontend - UI design v1.2 - Info component designs updated

--- frontend/src/app/components/info/info.component.ts
+++ frontend/src/app/components/info/info.component.ts
@@ -11,6 +11,9 @@ import { HttpClient } from '@angular/common/http';
 export class InfoComponent {
   name: string = "Danh Le";
   location: string = "Calgary, AB"
-  description_en: string  = "My name is Danh Le, a junior software developer based in Calgary, Alberta, Canada. I'm passionate about creating solutions that bring high values to the people around.";
+  description_1st: string  = "My name is Danh Le, a junior software developer based in Calgary, Alberta, Canada. I have recently graduated from my college but my learning journey has yet to stop, in fact, this is just the beginning. Curiosity and Perseverance are what guide me on the steps of becoming a software developer."; 
+  description_2nd: string = "I'm passionate about creating solutions that bring high values to the people around."
+  
+
 }
 
