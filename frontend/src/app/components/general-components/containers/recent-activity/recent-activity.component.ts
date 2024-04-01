84a793c686f5699c838ae47cfa1892b31a358485
Danh Le <danhle002@gmail.com>
2024-04-01 02:12:00 -0600

    frontend - UI design v1.2 - Github Component designs are updated

--- frontend/src/app/components/general-components/containers/recent-activity/recent-activity.component.ts
+++ frontend/src/app/components/general-components/containers/recent-activity/recent-activity.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { GithubComponent } from '../../../github/github.component';
 import { LeetcodeComponent } from '../../../leetcode/leetcode.component';
 import { MenuBarComponent } from '../../menu-bar/menu-bar.component';
+import { FooterBarComponent } from '../../footer-bar/footer-bar.component';
 
 @Component({
   selector: 'app-recent-activity',
   standalone: true,
-  imports: [GithubComponent, LeetcodeComponent, MenuBarComponent],
+  imports: [GithubComponent, LeetcodeComponent, MenuBarComponent, FooterBarComponent],
   templateUrl: './recent-activity.component.html',
   styleUrl: './recent-activity.component.css'
 })
