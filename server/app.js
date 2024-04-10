3273cd9bbedad68fc28166b28dcae81d74464b8f
Danh Le <danhle002@gmail.com>
2024-04-10 03:43:20 -0600

    server - enhanced server security

--- server/app.js
+++ server/app.js
@@ -10,11 +10,21 @@ const leetcode = require('./services/leetcodeService');
 const github = require('./services/githubService.js');
 const projects = require('./services/projectsService.js');
 
-app.use(cors());
-
 app.use(cors({
   origin: 'http://localhost:4200'
 }));
+
+app.use((req, res, next) => {
+  const protectedRoutes = ['/leetcode', '/github', '/projects'];
+  const allowedOrigins = ['http://localhost:4200'];
+  const origin = req.headers.referer;
+
+  if (protectedRoutes.includes(req.path) && (!origin || !allowedOrigins.some(allowedOrigin => origin.startsWith(allowedOrigin)))) {
+    return res.status(403).json({ message: 'Direct access not allowed' });
+  }
+
+  next();
+});
 
 
 app.get('/leetcode', leetcode.leetcodeGetInfo);
